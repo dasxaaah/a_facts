53f305d11724187b3864240fc10162b650adb4e3
Zay <z.znaranzaya@gmail.com>
2025-10-29 00:21:40 +0300

    user generated, added comment to post

--- db/seeds.rb
+++ db/seeds.rb
@@ -89,3 +89,25 @@ end
 
 puts "Q&A seed\n"
 # ============================================================================
+# create_users(10)
+
+# def create_users(quantity)
+#   i = 0
+
+#   quantity.times do
+#     user_data = {
+#       email: "user_#{i}@email.com",
+#       password: 'testtest'
+#     }
+
+#     user = User.create!(user_data)
+#     puts "User created with id #{user.id}"
+
+#     i += 1
+#   end
+# end
+
+author = User.first || User.create!(email: "user0@example.com", password: "testtest")
+Post.find_or_create_by!(title: "Какой софт для трекинга при параллаксе?") do |p|
+  p.body      = "Нужен совет по пайплайну (Nuke/AE, FBX и т.д.)."
+end
