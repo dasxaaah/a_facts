3178c869d6996c97293e7f157ee9ec6fabdd730b
Zay <z.znaranzaya@gmail.com>
2025-12-24 01:18:46 +0700

    user interface is done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
-  load_and_authorize_resource class: "Post"
-  # before_action :authenticate_user!
+  # load_and_authorize_resource class: "Post"
+  before_action :authenticate_user!
   # before_action :require_admin!
 
   def index
